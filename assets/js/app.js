6ae9c1ba8f77b534d4a899c9644ac79799914fcf
Yi Liu <elenaliu0415@gmail.com>
2021-10-02 23:32:54 -0700

    Fetch functions are working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,65 @@
 var weatherApiKey = "9ba2184f424558b17074bbc106ac975b";
 
 var form = $("#search-form");
-var searchInput = $("#search");
+var searchInput = $("#city-search");
 var selectedCityResult = $("#selected-result");
-var forcastContainer = $("#forcast-results");
+var forecastContainer = $("#forceast-results");
 
 // create a function to get user input from the form
 function handleFormSubmit(event) {
-  event.preventDefualt();
+  event.preventDefault();
   var query = searchInput.val().trim();
+
   if (query) {
     searchCity(query);
+
     searchInput.val("");
   }
 }
-
 // create a function to fetch data from the weather api
 function searchCity(query) {
-var url =
-  "api.openweathermap.org/data/2.5/weather?q=" +
-  query +
-  "&APPID=" +
-  weatherApiKey;
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&APPID=" +
+    weatherApiKey;
 
- fetch(url)
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  fetch(url)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      fetchWeatherData(data.coord);
+    });
 }
+
+function fetchWeatherData(result) {
+  var lat = result.lat;
+  var lon = result.lon;
+  var url =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon + 
+    "&units=imperial" +
+    "&exclude=minutely,hourly,alerts" +
+    "&appid=" +
+    weatherApiKey;
+
+    fetch(url)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+    });
+
+}
+
 // create a function to display selected city results
 
 // add event listener for form submit button
-form.on("submit", handleFormSubmit);
+form.on("click", handleFormSubmit);
