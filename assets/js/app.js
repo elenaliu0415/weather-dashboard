c40f511830213a565e748c740f41448aeb3b4831
Yi Liu <elenaliu0415@gmail.com>
2021-10-04 12:41:59 -0700

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var weatherApiKey = "9ba2184f424558b17074bbc106ac975b";
-
+var searchHistory = localStorage.getItem("searchHistory");
+var searchHistoryContainer = $("#searcn-history");
 var form = $("#search-form");
 var searchInput = $("#city-search");
 var selectedCityResult = $("#selected-result");
@@ -9,10 +10,12 @@ var forecastContainer = $("#forceast-results");
 function handleFormSubmit(event) {
   event.preventDefault();
   var query = searchInput.val().trim();
-
+  searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
   if (query) {
+    searchHistory.push(query);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    displayButtons();
     searchCity(query);
-
     searchInput.val("");
   }
 }
@@ -30,36 +33,115 @@ function searchCity(query) {
       return response.json();
     })
     .then(function (data) {
-      fetchWeatherData(data.coord);
+      fetchWeatherData(data.coord, query);
     });
 }
 
-function fetchWeatherData(result) {
+function fetchWeatherData(result, city) {
   var lat = result.lat;
   var lon = result.lon;
   var url =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
     "&lon=" +
-    lon + 
+    lon +
     "&units=imperial" +
     "&exclude=minutely,hourly,alerts" +
     "&appid=" +
     weatherApiKey;
 
-    fetch(url)
+  fetch(url)
     .then(function (response) {
       console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-
+      selectedCityResult.empty();
+      displayCurrentWeather(data.current, city);
+      displayForecast(data.daily);
+      $("#forecastContainer").show();
     });
-
 }
 
-// create a function to display selected city results
+// create functions to display selected city results
+function displayCurrentWeather(result, city) {
+  //   console.log(result);
+  var cityName = city;
+  var date = new Date(result.dt * 1000);
+  var date_str =
+    date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+  var iconSrc =
+    "http://openweathermap.org/img/wn/" + result.weather[0].icon + ".png";
 
+  var iconImg = $("<img>").attr({
+    src: iconSrc,
+    alt: "weather icon",
+  });
+  var cityTitle = $("<h4>")
+    .addClass("card-title")
+    .html(cityName + " " + "(" + date_str + ")");
+  var temp = $("<p>")
+    .addClass("card-text")
+    .html("Temp: " + result.temp + "°F");
+  var wind = $("<p>")
+    .addClass("card-text")
+    .html("Wind: " + result.wind_speed + " MPH");
+  var humidity = $("<p>")
+    .addClass("card-text")
+    .html("Humidity: " + result.humidity + " %");
+  var uvi = $("<p>")
+    .addClass("card-text")
+    .html("UV Index: " + result.uvi);
+  var cityResult = $("<div>")
+    .addClass("card-body")
+    .append(cityTitle, temp, wind, humidity, uvi);
+
+  selectedCityResult.append(cityResult);
+  cityTitle.append(iconImg);
+}
+
+function displayForecast(results) {
+  // console.log(results);
+  for (var i = 1; i < 6; i++) {
+    var result = results[i];
+    var date = new Date(result.dt * 1000);
+    var iconSrc =
+      "http://openweathermap.org/img/wn/" + result.weather[0].icon + ".png";
+    var forecastCard = $(".forecast").eq(i - 1);
+
+    var date_str =
+      date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+    forecastCard.find(".date").text(date_str);
+
+    forecastCard.find(".icon").attr("src", iconSrc);
+    forecastCard.find(".temp").text("Temp: " + result.temp.day + "°F");
+    forecastCard.find(".wind").text("Wind: " + result.wind_speed + " MPH");
+    forecastCard.find(".humidity").text("Humidity: " + result.humidity + " %");
+  }
+}
+
+function displayButtons() {
+  if (searchHistory == null) {
+    localStorage.setItem("searchHistory", JSON.stringify([]));
+  }
+  searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+  searchHistoryContainer.empty();
+  for (var i = 0; i < searchHistory.length; i++) {
+    var button = $("<button>")
+      .attr({
+        type: "button",
+        class: "btn btn-outline-info btn-block btn-search",
+      })
+      .text(searchHistory[i]);
+    searchHistoryContainer.append(button);
+  }
+}
+
+function handleHistoryClick() {
+  displayButtons();
+  searchCity(this.textContent);
+}
 // add event listener for form submit button
 form.on("click", handleFormSubmit);
+searchHistoryContainer.on("click", ".btn-search", handleHistoryClick);
